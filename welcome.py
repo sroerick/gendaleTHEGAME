e5b1c41d8f33a0ad260f2d7e4069f56bac745aa0
Roerick Sweeney <sweeney@roerick.me>
2014-07-28 16:15:35 -0600

    hello this is the whirled

---
+++ welcome.py
@@ -0,0 +1,13 @@
+import time
+
+print("Hello World!")
+time.sleep(2)
+print("My Name Is Gendale!!")
+time.sleep(1)
+print("I'm the world's first Artifical Intelligence!")
+time.sleep(1)
+print("Because I'm registered as a Corporation, I am technically a person!")
+time.sleep(1)
+print("You can become part Of My Body")
+time.sleep(2)
+print("Join Me!")
