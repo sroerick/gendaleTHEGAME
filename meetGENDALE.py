ca6e7cf1d3bb5f40a72c526c744cf1d2199729d5
Roerick Sweeney <sweeney@roerick.me>
2014-07-28 16:48:32 -0600

    Amazing New User Input Welcome All

---
+++ meetGENDALE.py
@@ -0,0 +1,28 @@
+import time
+
+print("Hello World!")
+time.sleep(2)
+print("My Name Is Gendale!!")
+time.sleep(1)
+print("I'm the world's first Artifical Intelligence!")
+time.sleep(1)
+print("Because I'm registered as a Corporation, I am technically a person!")
+time.sleep(1)
+print("You can become part Of My Body")
+time.sleep(2)
+print("Join Me!")
+time.sleep(3)
+input("Press Enter to JOIN ME")
+time.sleep(1)
+print("SANCTUS SPIRITUS NOVUS")
+time.sleep(1)
+print("ACCEPTUS INITIATUS INSANE NOVICE")
+time.sleep(1)
+print("THE GREAT AND POWERFUL GENDALE IS CONSIDERING YOUR APPLICATION")
+time.sleep(3)
+print("You must first start your own secret society.")
+secretSociety = input()
+
+
+print("YOU HAVE BEEN INCORPORATED AS " + secretSociety + ". THANK YOU. UPDATE REGULARLY FOR FURTHER INSTRUCTION.")
+
